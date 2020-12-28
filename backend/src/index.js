0a06273933486bb13da58bb3f9c15599e0d6d37f
Eduardo WB <eduardo@agenciacrow.com.py>
2020-12-28 12:37:38 -0300

    implement a memory based projects crud

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const { request, response } = require('express');
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -15,57 +16,70 @@ app.use(express.json());
  * DELETE: Deletar uma informação no backend
  */
 
- /**
-  * Tipos de parâmetros:
-  * 
-  * Query Params: Filtros e paginação
-  * Route Params: Identificar recursos (Atualizar/Deletar)
-  * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
-  */
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
+ */
+
+const projects = [];
 
 // Routes
 app.get('/projects', (request, response) => {
-    const {title, owner} = request.query;
+    const { title } = request.query;
 
-    console.log(title);
-    console.log(owner);
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-    ]);
+    return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
     const { title, owner } = request.body;
 
-    console.log(title);
-    console.log(owner);
+    const project = { id: uuid(), title, owner };
 
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    projects.push(project);
+
+    return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
     const { id } = request.params;
+    const { title, owner } = request.body;
 
-    console.log(id);
+    const projectIndex = projects.findIndex(project => project.id === id);
 
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).json();
 });
 
 // create http server, accessible by http://localhost:3333
