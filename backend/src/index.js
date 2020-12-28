daee73ef666780a0a35405b0a526e0dba778c40e
Eduardo WB <eduardo@agenciacrow.com.py>
2020-12-28 10:47:31 -0300

    implement hello world using express

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const { request, response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/projects', (request, response) => {
+    return response.send('Hello World');
+});
+
+app.listen(3333);
