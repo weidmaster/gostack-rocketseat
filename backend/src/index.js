6f799b95c5e50b6f1ae191753fb1b18a7ff28274
Eduardo WB <eduardo@agenciacrow.com.py>
2020-12-28 11:22:31 -0300

    implement all HTTP method examples

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,44 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do backend
+ * POST: Criar uma informação no backend
+ * PUT/PATCH: Alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ */
+
 // Routes
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+    ]);
 });
 
 // create http server, accessible by http://localhost:3333
