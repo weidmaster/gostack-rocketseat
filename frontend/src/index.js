afa6cde3d6ccf270be1352a2cd6ba4fac1dcb472
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-04 11:03:22 -0300

    implement Hello World using React

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,7 @@
-import {soma} from './soma';
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(soma(5, 2));
+// Para escrever HTML dentro do javascript, é necessário uar o '@babel/preset-react'
+
+// JSX: HTML dentro do Javascript (Javascript XML)
+render(<h1>Hello World</h1>, document.getElementById('app'));
