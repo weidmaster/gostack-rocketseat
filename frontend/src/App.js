bfaaf5165686b47277c79d0269fcdf3cd3f2e05c
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-04 11:12:55 -0300

    refactor the Hello World example to use React component

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// qualquer componente em React é uma função javascript que retorna HTML
+
+function App() {
+    return <h1>Hello GoStack</h1>;
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import App from './App'; // importando um componente React
+
 // Para escrever HTML dentro do javascript, é necessário uar o '@babel/preset-react'
 
 // JSX: HTML dentro do Javascript (Javascript XML)
-render(<h1>Hello World</h1>, document.getElementById('app'));
+
+// Qualquer componente React (função com inicial maiúscula) pode ser escrito como uma tag HTML
+render(<App />, document.getElementById('app'));
