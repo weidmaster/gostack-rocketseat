f935c23d374356bb95a7f875bf615481acbaf519
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-04 16:32:34 -0300

    import App.css as an example on how to handle css files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import './App.css';
+
 import Header from './components/Header';
 
 // qualquer componente em React é uma função javascript que retorna HTML
