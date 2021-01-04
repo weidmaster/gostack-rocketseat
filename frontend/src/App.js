cac85009853c5875e641d070ecc2b330201e53ae
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-04 11:27:49 -0300

    implement a Header component example with fragments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 // qualquer componente em React é uma função javascript que retorna HTML
 
 function App() {
-    return <h1>Hello GoStack</h1>;
+    /** Para podermos retornar vários componentes em sequência, precisamos agrupar
+    * os mesmos usando fragmentos, que são tags vazias que não causam efeito
+    * visual
+    */
+    return (
+        <>
+            <Header />
+            <Header />
+        </>
+    )
 }
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+    // para retornar um HTML com várias linhas, usamos os parênteses
+    return (
+        <header>
+            <h1>ReactJS</h1>
+        </header>
+    );
+}
