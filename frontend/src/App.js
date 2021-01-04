939d96a4cfccb39c3873a3af49d1a38854fa5d4c
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-04 11:40:51 -0300

    refactor the Header component to use properties

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,14 @@ import Header from './components/Header';
 
 // qualquer componente em React é uma função javascript que retorna HTML
 
+/**
+ * 3 conceitos principais para criar qualquer código em React
+ * 
+ * Componente
+ * Propriedade
+ * Estado
+ */
+
 function App() {
     /** Para podermos retornar vários componentes em sequência, precisamos agrupar
     * os mesmos usando fragmentos, que são tags vazias que não causam efeito
@@ -11,8 +19,8 @@ function App() {
     */
     return (
         <>
-            <Header />
-            <Header />
+            <Header title="Homepage" />
+            <Header title="Projects" />
         </>
     )
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-export default function Header() {
+// Componentes sempre recebem um parâmetro props, que pode ser desetruturado
+export default function Header(props) {
     // para retornar um HTML com várias linhas, usamos os parênteses
+
+    // para retornar javascript dentro do HTML, usamos interpolação com chaves
     return (
         <header>
-            <h1>ReactJS</h1>
+            <h1>{props.title}</h1>
         </header>
     );
 }
