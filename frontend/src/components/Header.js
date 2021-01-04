489387a6fceca9c61fdc216390135f27ff941325
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-04 11:42:12 -0300

    refactor the Header component to use properties desesctrutured

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 // Componentes sempre recebem um parâmetro props, que pode ser desetruturado
-export default function Header(props) {
+export default function Header({ title }) {
     // para retornar um HTML com várias linhas, usamos os parênteses
 
     // para retornar javascript dentro do HTML, usamos interpolação com chaves
     return (
         <header>
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
         </header>
     );
 }
