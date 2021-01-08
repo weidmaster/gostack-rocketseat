50ad1b21596fd2c6e75330f86c647660a720c1b2
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-08 11:52:27 -0300

    add appointments routes file and setup router

--- primeiro-projeto-node/src/routes/index.ts
+++ primeiro-projeto-node/src/routes/index.ts
@@ -1,4 +1,9 @@
 import { Router } from "express";
+import appointmentsRouter from "./appointments.routes";
 
 const routes = Router();
+
+// toda rota que comece com appointments passa o que vier depois para appointmentsRouter
+routes.use("/appointments", appointmentsRouter);
+
 export default routes;
