bd95a4ed1688bfb08fa87d04c22f07ef05355c7c
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-08 14:06:55 -0300

    install date-fns

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { v4 as uuid } from "uuid";
+import { startOfHour, parseISO } from "date-fns";
 
 const appointmentsRouter = Router();
 
@@ -9,10 +10,12 @@ const appointments = [];
 appointmentsRouter.post("/", (request, response) => {
     const { provider, date } = request.body;
 
+    const parsedDate = startOfHour(parseISO(date)); // parseISO - converte uma data em formato string para um objeto Date
+
     const appointment = {
         id: uuid(),
         provider,
-        date,
+        date: parsedDate,
     };
 
     appointments.push(appointment);
