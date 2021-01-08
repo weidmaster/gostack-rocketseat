a5b819e294a6cfe9ffd57700897f99edb1b7d4fe
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-08 14:00:01 -0300

    add create route for appointments resource

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,10 +1,23 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
 
 const appointmentsRouter = Router();
 
+const appointments = [];
+
 // POST http://localhost:3333/appointments
 appointmentsRouter.post("/", (request, response) => {
-    return response.json({ message: "Hello World" });
+    const { provider, date } = request.body;
+
+    const appointment = {
+        id: uuid(),
+        provider,
+        date,
+    };
+
+    appointments.push(appointment);
+
+    return response.json(appointment);
 });
 
 export default appointmentsRouter;
