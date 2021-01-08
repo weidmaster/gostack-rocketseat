e0cfc1d5547de6db8ee23b3ae4f481f4a2147796
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-08 14:20:33 -0300

    add business rules for creating an appointment and define types

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,16 +1,33 @@
 import { Router } from "express";
 import { v4 as uuid } from "uuid";
-import { startOfHour, parseISO } from "date-fns";
+import { startOfHour, parseISO, isEqual } from "date-fns";
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+// Quando temos erros do typo any[] no typescript, precisamos definir a tipagem.
+// usamos interface para tipar dados complexos
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointment[] = []; // define um array de Appointment
 
 // POST http://localhost:3333/appointments
 appointmentsRouter.post("/", (request, response) => {
     const { provider, date } = request.body;
 
     const parsedDate = startOfHour(parseISO(date)); // parseISO - converte uma data em formato string para um objeto Date
+    const findAppointmentInSameDate = appointments.find((appointment) =>
+        isEqual(parsedDate, appointment.date)
+    );
+
+    if (findAppointmentInSameDate) {
+        return response
+            .status(400)
+            .json({ message: "This appointment is already booked" });
+    }
 
     const appointment = {
         id: uuid(),
