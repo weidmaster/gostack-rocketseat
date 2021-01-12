a73ada1abfb6ecf0115997a2b271ba9db1a76a23
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-12 17:48:10 -0300

    refactor to crypto user password with bcryptjs

--- primeiro-projeto-node/src/routes/users.routes.ts
+++ primeiro-projeto-node/src/routes/users.routes.ts
@@ -16,7 +16,17 @@ usersRouter.post('/', async (request, response) => {
             password,
         });
 
-        return response.json(user);
+        // delete user.password não funciona mais no typescript novo
+
+        const userWithoutPassword = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        };
+
+        return response.json(userWithoutPassword);
     } catch (err) {
         return response.status(400).json({ error: err.message });
     }
