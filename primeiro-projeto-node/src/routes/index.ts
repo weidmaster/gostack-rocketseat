f4a973e5c11daf21ed5152139ac539daabd36375
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-12 17:13:52 -0300

    add User model and service

--- primeiro-projeto-node/src/routes/index.ts
+++ primeiro-projeto-node/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 // toda rota que comece com appointments passa o que vier depois para appointmentsRouter
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
