ded2d8db68726eaec6169dfc11bad8bd6a744675
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-12 10:27:04 -0300

    refactor Appointment model to be a typeorm Entity

--- primeiro-projeto-node/src/models/Appointment.ts
+++ primeiro-projeto-node/src/models/Appointment.ts
@@ -1,19 +1,32 @@
-import { v4 as uuid } from 'uuid';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+// chamamos de Entidade algo que será salvo no banco de dados
+
+/**
+ * Decorators
+ * São expressões iniciadas com arroba (@)
+ * Basicamente são funções que recebem a classe inteira como parâmetro
+ *
+ * @Entity('table') - indica para salvar as informações dentro de uma tabela no banco de dados
+ * É necessário informar com outros decorators quais propriedades são colunas no banco de dados.
+ * Dessa forma é possível ter propriedades que não correspondem e não serão salvas.
+ *
+ * @PrimaryGeneratedColumn() - indica a chave primária e a forma dessa chave
+ * @Column() - indica uma coluna e seu formato. O padrão é varchar
+ */
+
+@Entity('appointments')
 class Appointment {
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Column()
     provider: string;
 
+    @Column('timestamp with time zone')
     date: Date;
 
-    // Ao utilizar parâmetros nomeados aproveitando a estrutura de dados já definida, devemos usar o Omit do Typescript
-    // para excluir propriedades que são geradas e não passadas pela aplicação
-    constructor({ provider, date }: Omit<Appointment, 'id'>) {
-        this.id = uuid();
-        this.provider = provider;
-        this.date = date;
-    }
+    // não é necessário mais utilizar o construtor em uma classe do tipo Entidade do typeorm
 }
 
 export default Appointment;
