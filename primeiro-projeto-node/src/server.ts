425da6f93d1d5cf47853e69a4de40603cb289a73
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-13 14:46:10 -0300

    apply custom error class as global error handling for express

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,7 +1,10 @@
 /* eslint-disable no-console */
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -10,6 +13,24 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+// Middleware de tratamento de erros global
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        // Se o erro foi enviado pela aplicação, retorna de forma amigável para o frontend
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error',
+    });
+});
 
 app.listen(3333, () => {
     console.log('🚀 Server started on port 3333');
