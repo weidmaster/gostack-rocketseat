88754dd851ef4a5a323bbd645a527758c166d71f
Eduardo WB <eduardo@agenciacrow.com.py>
2021-01-13 12:01:30 -0300

    save uploaded avatar to database

---
+++ primeiro-projeto-node/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+import uploadConfig from '../config/upload';
+
+import User from '../models/User';
+
+interface Request {
+    user_id: string;
+    avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+    public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne(user_id);
+
+        if (!user) {
+            throw new Error('Only authenticated users can change avatar');
+        }
+
+        if (user.avatar) {
+            // Deletar avatar anterior
+
+            const userAvatarPath = path.join(
+                uploadConfig.directory,
+                user.avatar,
+            );
+            const userAvatarFileExists = await fs.promises.stat(userAvatarPath);
+
+            if (userAvatarFileExists) {
+                await fs.promises.unlink(userAvatarPath);
+            }
+        }
+
+        user.avatar = avatarFilename;
+
+        await usersRepository.save(user); // se o id existir atualiza
+
+        return user;
+    }
+}
+
+export default UpdateUserAvatarService;
